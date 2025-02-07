cf5b93f729c176ce67d69a226072ff594aeb4160
simon-persson1 <simon.persson@umain.com>
2025-02-07 17:13:13 +0100

    further developed comments on variables

--- src/RainScene.tsx
+++ src/RainScene.tsx
@@ -3,27 +3,34 @@ import * as THREE from "three";
 
 const RainScene: React.FC = () => {
   const mountRef = useRef<HTMLDivElement | null>(null);
+  //   const green = "#22577A";
+  //   const blue = "#38A3A5";
+  //   const yellow = "#57CC99";
+  //   const orange = "#E94F37";
+  //   const red = "#D9594C";
+  //   const white = "#F9F8F8";
 
   // Adjustable parameters
-  const fogDensity = 0.002; //Cloud and rain visibility ranging from 0.006 (no clouds) to 0.001 (dense clouds) EXTRA: 0.5 to hide rain and clouds
+  const fogDensity = 0.0015; //Cloud and rain visibility ranging from 0.006 (no clouds) to 0.001 (dense clouds) EXTRA: 0.5 to hide rain and clouds
   const cameraPosition = { x: 0, y: 0, z: 1 };
   const cameraRotation = { x: 1.16, y: -0.12, z: 0.27 };
-  const ambientLightIntensity = 1;
-  const directionalLightIntensity = 0.5;
+  const ambientLightIntensity = 0.1; // how thick the clouds will be, 0.1 for light clouds and 100 for dense clouds
+  const directionalLightIntensity = 50; // Light intensity ranging from 0 (no light) to 50 (brightest light) color intensity for the clouds at 50 with cloudColor as white makes them white, suited for when cloudy but daytime and sunny
   const flashColor = 0x062d89;
   const flashIntensity = 30;
-  const flashDistance = 500;
+  const flashDistance = 10;
   const flashDecay = 1.7;
-  const rainCount = 9500; // Number of raindrops
+  const rainCount = 10000; // Number of raindrops ranging from 0( no rain) to 10000 (heavy rain), 500 for light rain
   const rainColor = 0xaaaaaa; // Raindrop color
-  const rainSize = 0.1; // Raindrop size 
-  const cloudOpacity = 0.6; // Cloud opacity
+  const rainSize = 0.1; // Raindrop size going from 0.1 to 1, 0.1 being rain and 1 being snow, higher the number, the closer to snow
+  const cloudOpacity = 1; // Cloud opacity
   const cloudCount = 25; // Number of clouds (Max 200)
-
+  const skyColor = 0x87ceeb; // Sky color in hex, 0x11111f for night sky and 0x87ceeb for day sky (light blue)
+  const cloudColor = 0xffffff; // Cloud color in hex, 0x111111 for dark clouds and 0xffffff for white clouds
   useEffect(() => {
     // Scene setup
     const scene = new THREE.Scene();
-    scene.fog = new THREE.FogExp2(0x11111f, fogDensity);
+    scene.fog = new THREE.FogExp2(skyColor, fogDensity);
 
     // Camera setup
     const camera = new THREE.PerspectiveCamera(
@@ -46,11 +53,11 @@ const RainScene: React.FC = () => {
     }
 
     // Lights
-    const ambient = new THREE.AmbientLight(0x555555, ambientLightIntensity);
+    const ambient = new THREE.AmbientLight(cloudColor, ambientLightIntensity);
     scene.add(ambient);
 
     const directionalLight = new THREE.DirectionalLight(
-      0xffeedd,
+      cloudColor,
       directionalLightIntensity
     );
     directionalLight.position.set(0, 0, 1);
@@ -62,7 +69,7 @@ const RainScene: React.FC = () => {
       flashDistance,
       flashDecay
     );
-    flash.position.set(200, 300, 100);
+    flash.position.set(window.innerWidth / 2, window.innerHeight / 2, 100);
     scene.add(flash);
 
     // Rain setup
